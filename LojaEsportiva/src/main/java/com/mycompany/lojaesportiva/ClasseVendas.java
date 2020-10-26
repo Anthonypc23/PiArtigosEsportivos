2dcb65dc2d0997e546e0e8cb2ed43659a7e9323e
WesleyaAlex <wesley132013@gmail.com>
2020-10-26 20:17:47 -0300

    Fiz as alterações pedidas no GitHub.

---
+++ LojaEsportiva/src/main/java/com/mycompany/lojaesportiva/ClasseVendas.java
@@ -0,0 +1,92 @@
+
+package com.mycompany.lojaesportiva;
+
+public class ClasseVendas {
+    
+    private String cliente;
+    private String produto;
+    private int descricao;
+    private float preco;
+    private int quantidade;
+    private String servicos;
+    private String tipoParcela;
+    private String marca;
+    private String tamanho;
+
+    public ClasseVendas() {
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getProduto() {
+        return produto;
+    }
+
+    public void setProduto(String produto) {
+        this.produto = produto;
+    }
+
+    public int getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(int descricao) {
+        this.descricao = descricao;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getServicos() {
+        return servicos;
+    }
+
+    public void setServicos(String servicos) {
+        this.servicos = servicos;
+    }
+
+    public String getTipoParcela() {
+        return tipoParcela;
+    }
+
+    public void setTipoParcela(String tipoParcela) {
+        this.tipoParcela = tipoParcela;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    
+}
